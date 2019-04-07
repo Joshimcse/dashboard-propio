85bdce7ddd3e005ac39a5c4911ea1984b8640a98
Nicolás Morales <nicomorales297@gmail.com>
2019-04-07 17:02:04 -0400

    rollback a context

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
-import {Home,Categorias} from './pages/index.pages'
+import {Categorias} from './pages/index.pages'
 
 export default{
     paginas:[
-        {
+        /*{
             path: '/',
             nombre: 'Home',
             tipo: 'link',
             component: Home
-        },
+        },*/
         {
-            path: '/categorias',
-            nombre: 'Categorias',
+            path: '/',
+            nombre: 'Home',
             tipo: 'link',
             component: Categorias
         }
